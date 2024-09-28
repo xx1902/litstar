486efe2a040897869d444dff7d957a5ae83529cc
小星 <1736357992@qq.com>
2024-09-28 23:24:57 +0800

    更新title样式

--- js/title.js
+++ js/title.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '👀跑哪里去了~';
+    document.title = '🥺你还会回来吗';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '🐖抓到你啦～';
+    document.title = '🥰欢迎回家';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
